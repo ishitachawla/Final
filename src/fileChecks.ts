0e2a2f652305e46237d967e1ef048b81bff7c903
Ishita Chawla <84702817+ishitachawla@users.noreply.github.com>
2021-07-13 07:17:35 +0000

    readme

--- src/fileChecks.ts
+++ src/fileChecks.ts
@@ -2,99 +2,108 @@ import * as core from '@actions/core';
 import * as fs from 'fs';
 import { Octokit } from '@octokit/core';
 
-export function readmeChecks(files: string[]){
-    const includesReadme = files.includes('README.md');
-    if(includesReadme){
-        console.log('Success - README file is present');
-        fs.readFile('./README.md', function (err, data) {
-            //check example
-            if(data.includes('Example')){
-                console.log('Success - Example workflow is present in README');
-            }
-            else{
-                core.setFailed('Please add Example workflow in README');
-            }            
-            //check contribution
-            if(data.includes('Contribution') || data.includes('Contributing')){
-                console.log('Success - Contribution Guidelines are present in README');
-            }             
-            else{
-                core.setFailed('Please add Contribution in README');
-            }
-        });
-    }
-    else{
-        core.setFailed('Please add README file');
-    }
+export async function readmeChecks(repository: string, ownername: string, secret_token: string, octokit: Octokit) {
+	try {
+		const result = await octokit.request('GET /repos/{owner}/{repo}/readme', {
+			repo: repository,
+			owner: ownername,
+			headers: {
+				Authorization: 'Bearer ' + secret_token
+			}
+		});
+		if (result.status == 200) {
+			console.log('README file is present');
+			const current = await octokit.request('GET /repos/{owner}/{repo}/contents/README.md', {
+				repo: repository,
+				owner: ownername,
+				headers: {
+					Authorization: 'Bearer ' + secret_token
+				}
+			});
+			let contents = Buffer.from(current.data.content, "base64").toString("utf8");
+			if (contents.includes('Example')) {
+				console.log('Example workflow is present')
+			}
+		}
+		else{
+			core.setFailed('Please add Readme')
+		}
+	}
+	catch (err) {
+		console.log(err);
+	}
+
 }
-  
-export function codeOwnerCheck(){
-    fs.readdir('./.github', (err, files) => {
-        const includesCodeOwners = files.includes('CODEOWNERS');
-        if(includesCodeOwners){
-            console.log('Success - CODEOWNERS file is present');
-        }
-        else{
-            core.setFailed('Please add CODEOWNERS file');
-        }
-    })
+
+export function codeOwnerCheck() {
+	fs.readdir('./.github', (err, files) => {
+		const includesCodeOwners = files.includes('CODEOWNERS');
+		if (includesCodeOwners) {
+			console.log('Success - CODEOWNERS file is present');
+		}
+		else {
+			core.setFailed('Please add CODEOWNERS file');
+		}
+	})
 }
-  
+
 function getExtension(filename: string) {
-    return filename.substring(filename.lastIndexOf('.') + 1, filename.length)   
+	return filename.substring(filename.lastIndexOf('.') + 1, filename.length)
 }
-  
-export function nodeModulesCheck(){
-    fs.readdir('./src',(err, filelist ) => {
-        for(let i = 0; i < filelist.length; i++){
-            if(getExtension(filelist[i]) === 'ts'){
-                fs.readdir('./', (err, files) => {
-                    const includes_node_modules = files.includes('node_modules');
-                    if(includes_node_modules){
-                        core.setFailed('Please remove node_modules folder from master');
-                    }
-                    else{
-                        console.log('Success - node_modules folder is not present in master');
-                    }
-                })
-                break;
-            }
-        }
-    })
+
+export function nodeModulesCheck() {
+	fs.readdir('./src', (err, filelist) => {
+		for (let i = 0; i < filelist.length; i++) {
+			if (getExtension(filelist[i]) === 'ts') {
+				fs.readdir('./', (err, files) => {
+					const includes_node_modules = files.includes('node_modules');
+					if (includes_node_modules) {
+						core.setFailed('Please remove node_modules folder from master');
+					}
+					else {
+						console.log('Success - node_modules folder is not present in master');
+					}
+				})
+				break;
+			}
+		}
+	})
 }
-  
-export async function releasesNodeModulesCheck(repository: string, ownername: string, secret_token: string, octokit: Octokit){ 
-    try{
-        const result = await octokit.request('GET /repos/{owner}/{repo}/branches',{
-        owner: ownername,
-        repo: repository,
-        headers : { Authorization: 'Bearer ' + secret_token
-        }
-        });
-        for(let i=0;i<result.data.length;i++){
-            if(result.data[i].name.substring(0,9) === 'releases/'){
-                var branchname = result.data[i].name;
-                const branch = await octokit.request('GET /repos/{owner}/{repo}/contents',{
-                owner: ownername,
-                repo: repository,
-                ref: branchname,
-                headers : { Authorization: 'Bearer ' + secret_token
-                }
-                })
-                var flag=0;
-                for(let j=0;j<branch.data.length;j++){
-                    if(branch.data[j].name === 'node_modules'){
-                        flag=1;
-                        console.log('Success - node_modules folder is present in '+ branchname);
-                    } 
-                }
-                if(flag===0){
-                    core.setFailed('Please add node_modules to '+ branchname);
-                }
-            }
-        }
-    }
-    catch(err){
-        console.log(err);
-    }       
+
+export async function releasesNodeModulesCheck(repository: string, ownername: string, secret_token: string, octokit: Octokit) {
+	try {
+		const result = await octokit.request('GET /repos/{owner}/{repo}/branches', {
+			owner: ownername,
+			repo: repository,
+			headers: {
+				Authorization: 'Bearer ' + secret_token
+			}
+		});
+		for (let i = 0; i < result.data.length; i++) {
+			if (result.data[i].name.substring(0, 9) === 'releases/') {
+				var branchname = result.data[i].name;
+				const branch = await octokit.request('GET /repos/{owner}/{repo}/contents', {
+					owner: ownername,
+					repo: repository,
+					ref: branchname,
+					headers: {
+						Authorization: 'Bearer ' + secret_token
+					}
+				})
+				var flag = 0;
+				for (let j = 0; j < branch.data.length; j++) {
+					if (branch.data[j].name === 'node_modules') {
+						flag = 1;
+						console.log('Success - node_modules folder is present in ' + branchname);
+					}
+				}
+				if (flag === 0) {
+					core.setFailed('Please add node_modules to ' + branchname);
+				}
+			}
+		}
+	}
+	catch (err) {
+		console.log(err);
+	}
 }
--- src/main.ts
+++ src/main.ts
@@ -1,43 +1,37 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
-import * as fs from 'fs';
 import { Octokit } from '@octokit/core';
-import {readmeChecks, codeOwnerCheck, nodeModulesCheck, releasesNodeModulesCheck} from './fileChecks'
-import {branchPermissionCheck} from './branchPermissionCheck'
-import {vulnerabilityBotCheck} from './vulnerabilityBotCheck'
-import {issueTemplateCheck} from './issueTemplateCheck'
-import {standardLabelsCheck} from './standardLabelsCheck'
+import { readmeChecks, codeOwnerCheck, nodeModulesCheck, releasesNodeModulesCheck } from './fileChecks'
+import { branchPermissionCheck } from './branchPermissionCheck'
+import { vulnerabilityBotCheck } from './vulnerabilityBotCheck'
+import { issueTemplateCheck } from './issueTemplateCheck'
+import { standardLabelsCheck } from './standardLabelsCheck'
 
-async function main() { 
-  fs.readdir('./', (err, files) => {
-    if (err){
-      console.log(err);
-    }
-    else{
-      var secret_token = core.getInput('GITHUB_TOKEN');
-      const octokit = new Octokit({
-      auth: secret_token,
-      });
-      const repository = github.context.repo.repo;
-      const ownername = github.context.repo.owner;
-      //Check for example and Contribution in README
-      readmeChecks(files);
-      //Check for CODEOWNERS file in .github folder
-      codeOwnerCheck();
-      //Check if nodemodules folder is present in master branch for typescript action
-      nodeModulesCheck();
-      //check for branch permissions in main/master and releases/*
-      branchPermissionCheck(repository, ownername, secret_token, octokit);
-      //check for nodemodules folder in releases/*
-      releasesNodeModulesCheck(repository, ownername, secret_token, octokit);
-      //check for security/vulnerability bot
-      vulnerabilityBotCheck(repository, ownername, secret_token, octokit);
-      //1. check whether issue-template has been set up and 2. default label is need-to-triage
-      issueTemplateCheck();
-      //Check whether standard labels have been set up
-      standardLabelsCheck(repository, ownername, secret_token, octokit)
-    }
-  })
+async function main() {
+
+	var secret_token = core.getInput('GITHUB_TOKEN');
+	const octokit = new Octokit({
+		auth: secret_token,
+	});
+	const repository = github.context.repo.repo;
+	const ownername = github.context.repo.owner;
+	//Check for example and Contribution in README
+	readmeChecks(repository, ownername, secret_token, octokit);
+	//Check for CODEOWNERS file in .github folder
+	codeOwnerCheck();
+	//Check if nodemodules folder is present in master branch for typescript action
+	nodeModulesCheck();
+	//check for branch permissions in main/master and releases/*
+	branchPermissionCheck(repository, ownername, secret_token, octokit);
+	//check for nodemodules folder in releases/*
+	releasesNodeModulesCheck(repository, ownername, secret_token, octokit);
+	//check for security/vulnerability bot
+	vulnerabilityBotCheck(repository, ownername, secret_token, octokit);
+	//1. check whether issue-template has been set up and 2. default label is need-to-triage
+	issueTemplateCheck();
+	//Check whether standard labels have been set up
+	standardLabelsCheck(repository, ownername, secret_token, octokit)
+
 }
 
 main();
